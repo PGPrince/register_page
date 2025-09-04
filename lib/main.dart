cad8e2bb4e83f5f2b974997913fe4e8dfe25ba4c
unknown <ghoshprince999@gmail.com>
2025-09-04 13:14:05 +0600

    register_page

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Color(0xffFFFFFF),
         body: SafeArea(
           child: Column(
             children: [
@@ -63,7 +64,7 @@ class MyApp extends StatelessWidget {
                       borderRadius: BorderRadius.circular(20),
                     ),
                     enabledBorder: OutlineInputBorder(
-                       borderSide: BorderSide(color: Color(0xff8FA31E)),
+                       borderSide: BorderSide(color: Color(0xff556B2F)),
                        borderRadius: BorderRadius.circular(20),
                     ),
                   ),
@@ -77,8 +78,121 @@ class MyApp extends StatelessWidget {
                 child: TextFormField(
                   decoration: InputDecoration(
                     hintText: 'Phone',
+                    fillColor: Color(0xffC6D870),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff556B2F)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff556B2F)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
                 ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    fillColor: Color(0xffC6D870),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff556B2F)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff556B2F)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Confirm Password',
+                    fillColor: Color(0xffC6D870),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff556B2F)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff556B2F)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 40,
+                width: 380,
+                decoration: BoxDecoration(
+                  color: Color(0xff556B2F),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Inter-Bold',
+                      color: Color(0xffFFFFFF),
+                    ),
+                    ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 380,
+                    decoration: BoxDecoration(
+                      color: Color(0xff556B2F),
+                      borderRadius: BorderRadius.circular(20),
+                ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Have an account?',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontFamily: 'Inter-Bold',
+                            color: Color(0xffFFFFFF),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Sign In', 
+                          style: TextStyle(
+                            fontSize: 18, 
+                            fontFamily: 'Inter-Bold',
+                            color: Color(0xffFFFFFF),
+                          ),
+                        ),
+                      ],
+                    ),
+              ),
+              ],
               ),
             ],
           ),
