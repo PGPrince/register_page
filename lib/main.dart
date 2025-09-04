c8a8de41b00c7d5eb4a99b5844e038457ef9b6df
unknown <ghoshprince999@gmail.com>
2025-09-04 12:00:09 +0600

    register_page

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,74 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
           child: Column(
             children: [
-              Text(
-                'Register',
-                style: TextStyle(
-                  fontSize: 44,
-                  fontFamily: 'Inter-Bold',
+              Center(
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    fontSize: 44,
+                    fontFamily: 'Inter-Bold',
+                    color: Color(0xff556B2F)
+                  ),
+                ),
+              ),
+              SizedBox(
+                    height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Full Name',
+                    fillColor: Color(0xffC6D870),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff556B2F),
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff556B2F)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    fillColor: Color(0xffC6D870),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff556B2F)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                       borderSide: BorderSide(color: Color(0xff8FA31E)),
+                       borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Phone',
+                  ),
                 ),
               ),
             ],
